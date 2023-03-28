85228152f28b0f5e5b27bbea8140e93e70d66525
YADHU2004 <124909554+YADHU2004@users.noreply.github.com>
2023-03-28 15:09:13 +0530

    Create embedded for led blinking.c

---
+++ embedded for led blinking.c
@@ -0,0 +1,24 @@
+#include <AT89S52.h>
+
+void wait(void);
+
+void main()
+{
+
+ while(1)
+ {
+  P2_0=0;
+  wait();
+  P2_0=1;
+  wait();
+ }
+}
+
+
+
+void wait(void)
+{
+int i;
+for(i=0;i<100;i++)
+{}
+}
